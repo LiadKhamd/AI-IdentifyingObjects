3a80171e760f84265693750bf31886468af0ef1f
Liad Khamd <liadkh95@gmail.com>
2018-11-14 09:39:47 +0200

    flower template

---
+++ AI-IdentifyingFlowers/Functions.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "GLUT.h"
+#include <math.h>
+
+#include <stdio.h>
+#include <time.h>
+#include <Windows.h>
+
+const int W = 600; // window width
+const int H = 600; // window height
+
+const int TMPSZ = 512;
+const int SCRSZ = 256;
+const int INPUT_SZ = SCRSZ + 1;//1-bios
+const int HIDDEN_SZ = INPUT_SZ / 2 + 1;//1-bios
+const int OUTPUT_SZ = 3;
+const static char* FLOWERS[OUTPUT_SZ] = { "chrysanthemum", "rose", "tulip"};
+
+void LoadBitmap(char * filename);
+void init();
+void DrawSquares();
+void HPF();
+int MaxOutput();
+void Clean();
+void FeedForward();
+void Backpropagation();
+void display();
+void idle();
+void drag(int x, int y);
+
+void mouse(int button, int state, int x, int y);
--- AI-IdentifyingFlowers/Main.cpp
+++ AI-IdentifyingFlowers/Main.cpp
@@ -1,3 +1,16 @@
-int main(int argc, char* argv[]) {
+#include "Functions.h"
 
+int main(int argc, char* argv[]) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutInitWindowSize(W, H);
+	glutInitWindowPosition(200, 100);
+	glutCreateWindow("AI-IdentifyingNumbers");
+
+	glutDisplayFunc(display); // refresh function
+	glutIdleFunc(idle); // idle: when nothing happens
+	glutMouseFunc(mouse);
+	init();
+
+	glutMainLoop();
 }
